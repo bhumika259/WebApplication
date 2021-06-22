064c006a6eff3175c1161d52c73904a039536951
Bhumika Jatav <bhumijatav259@outlook.com>
2021-06-22 20:54:14 +0530

    Add project files.

---
+++ Models/ViewModel/ViewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models.ViewModel
+{
+    
+    public class NewMessageViewModel
+    {
+        public int Id { get; set; }
+        public object Username { get; internal set; }
+        public string MessageBody { get; internal set; }
+        public DateTime? PostDateTime { get; set; }
+    }
+}
